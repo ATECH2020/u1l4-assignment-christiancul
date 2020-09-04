6e173c7a816448370b11560f80f92812fbe4a4cb
Christian Culanag <christia.821792@nv.ccsd.net>
2020-09-04 06:05:14 +0000

    Added code to calculate amt of hrs or minutes past midnight

--- src/main/java/SecondsPastMidnight.java
+++ src/main/java/SecondsPastMidnight.java
@@ -2,16 +2,25 @@ import java.util.Scanner;
 
 class SecondsPastMidnight {
     public static void main(String[] args) {
-
         //Creates a new scanner for user input
         Scanner scanner = new Scanner(System.in);
-
+        System.out.print("Please enter the amt of seconds: ");
         //Reads int from user
-        int number = scanner.nextInt();
-
+        int seconds = scanner.nextInt();
+        
         //Your code goes here
+        int hours, minutes;
+        hours = minutes = 0;
 
+        //Convert secs into minutes
+        minutes = seconds / 60;
+        
+        //Convert minutes into hours
+        hours = minutes / 60;
 
+        //Print out hrs && minutes passed
+        //System.out.printf("%d hrs or %d minutes has passed\n", hours, minutes);
+        System.out.println(hours + " " + minutes);
         // closing the scanner object
         scanner.close();
     }
