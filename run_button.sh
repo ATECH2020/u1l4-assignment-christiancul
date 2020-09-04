5b502525e1cb3aa7151efaa6347f99639ca66c45
Christian Culanag <christia.821792@nv.ccsd.net>
2020-09-04 21:09:36 +0000

    Removed print statements to test autograder && added timestamps to run_button.sh

--- run_button.sh
+++ run_button.sh
@@ -3,3 +3,4 @@ javac src/main/java/AnalogClock.java
 javac src/main/java/StudentDesks.java
 javac src/main/java/Timestamps.java
 java -cp src/main/java/ SecondsPastMidnight
+java -cp src/main/java/ Timestamps
--- src/main/java/SecondsPastMidnight.java
+++ src/main/java/SecondsPastMidnight.java
@@ -4,19 +4,16 @@ class SecondsPastMidnight {
     public static void main(String[] args) {
         //Creates a new scanner for user input
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Please enter the amt of seconds: ");
+        //System.out.print("Please enter the amt of seconds: ");
         //Reads int from user
         int seconds = scanner.nextInt();
         
         //Your code goes here
-        int hours, minutes;
-        hours = minutes = 0;
-
         //Convert secs into minutes
-        minutes = seconds / 60;
+        int minutes = seconds / 60;
         
         //Convert minutes into hours
-        hours = minutes / 60;
+        int hours = minutes / 60;
 
         //Print out hrs && minutes passed
         //System.out.printf("%d hrs or %d minutes has passed\n", hours, minutes);
