b62759b04434dfbf048bfe4f234eaf1b571b3d7d
Christian Culanag <christia.821792@nv.ccsd.net>
2020-09-04 22:23:44 +0000

    fixed StudentDesks

--- src/main/java/StudentDesks.java
+++ src/main/java/StudentDesks.java
@@ -14,12 +14,11 @@ class StudentDesks {
         /*
          *  your code goes here
          */
-        int studentsAmtTotal = studentsAmtA + studentsAmtB + studentsAmtC;
-
-        int desksNeeded = studentsAmtTotal / 2;
-
-        if (desksNeeded % 2 == 1) { desksNeeded++; }
-
+        int desksNeededA = (studentsAmtA / 2) + (studentsAmtA % 2);
+        int desksNeededB = (studentsAmtB / 2) + (studentsAmtB % 2);
+        int desksNeededC = (studentsAmtC / 2) + (studentsAmtC % 2);
+        
+        int desksNeeded = desksNeededA + desksNeededB + desksNeededC;
         System.out.print(desksNeeded);
         // closing the scanner object
         scanner.close();
