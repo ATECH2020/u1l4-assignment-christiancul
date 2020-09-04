9f7c706cabbefe515a4f50bec891a9063d75d0f6
Christian Culanag <christia.821792@nv.ccsd.net>
2020-09-04 22:58:29 +0000

    implemented analog clock

--- run_button.sh
+++ run_button.sh
@@ -4,4 +4,5 @@ javac src/main/java/StudentDesks.java
 javac src/main/java/Timestamps.java
 # java -cp src/main/java/ SecondsPastMidnight
 # java -cp src/main/java/ Timestamps
-java -cp src/main/java/ StudentDesks
+# java -cp src/main/java/ StudentDesks
+java -cp src/main/java/ AnalogClock
--- src/main/java/AnalogClock.java
+++ src/main/java/AnalogClock.java
@@ -6,9 +6,19 @@ class AnalogClock {
         //Creates a new scanner for user input
         Scanner scanner = new Scanner(System.in);
 
-        //Reads ints from user
+        //Reads ints from users
+        int hrAngle = scanner.nextInt();
 
+        //calc hr (rounded)
+        int hr = (int) ((hrAngle / 360.0) * 12.0);
 
+        //subtract hours by hr (rounded) in order to get ratio for minutes, convert ratio into 1-12 on the clock
+        int minute = (int) ((((hrAngle / 360.0) * 12.0) - hr) * 12);
+        //convert clock num to angle
+        int minuteAngle = minute * 30;
+
+        //print angle
+        System.out.println(minuteAngle);
         /*
          *  your code goes here
          */
