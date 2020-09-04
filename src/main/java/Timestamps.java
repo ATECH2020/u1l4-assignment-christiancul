e5e59ac0294949b6bb77a945b4d1bed198b8b40a
Christian Culanag <christia.821792@nv.ccsd.net>
2020-09-04 21:01:17 +0000

    Implemented Timestamps class

--- src/main/java/Timestamps.java
+++ src/main/java/Timestamps.java
@@ -7,19 +7,36 @@ class Timestamps {
         Scanner scanner = new Scanner(System.in);
 
         //Reads ints from user
+        System.out.print("Hours 1: ");
         int hours1 = scanner.nextInt();
+        
+        System.out.print("Minutes 1: ");
         int minutes1 = scanner.nextInt();
+        
+        System.out.print("Seconds 1: ");
         int seconds1 = scanner.nextInt();
+        
+        System.out.print("Hours 2: ");
         int hours2 = scanner.nextInt();
+        
+        System.out.print("Minutes 2: ");
         int minutes2 = scanner.nextInt();
+        
+        System.out.print("Seconds 2: ");
         int seconds2 = scanner.nextInt();
+        
+        //convert change in hrs to secs
+        int hoursToSeconds = (hours2 - hours1) * 3600;
+        //convert change in minutes to secs
+        int minutesToSeconds = (minutes2 - minutes1) * 60;
+        //calc change in secs
+        int secondsToSeconds = (seconds2 - seconds1);
 
+        //add all seconds together
+        int totalSeconds = hoursToSeconds + minutesToSeconds + secondsToSeconds;
 
-        /*
-         *  your code goes here
-         */
-
-
+        //display seconds
+        System.out.println(totalSeconds);
         // closing the scanner object
         scanner.close();
     }
